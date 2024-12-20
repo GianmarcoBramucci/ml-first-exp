279e72738fb7731c49d8338cf53ddfdcc6e96924
GianmarcoBramucci <giangian797@gmail.com>
2024-12-20 17:21:04 +0100

    strating crating class base funcion

--- prova.py
+++ prova.py
@@ -1,1 +1,22 @@
 import numpy as np
+
+class DecionTree:
+    def __init__(self,maxProf=3):
+        self.maxDeap=maxProf
+        self.tree= None
+class Node:
+    def __init__(self,feat,des,sin,val,sol):
+        self.feat=feat
+        self.des= des
+        self.sin=sin
+        self.val= val
+        self.sol= sol
+    def _calculate_gini(self, y):
+        classi,count=  np.unique(y, return_counts=True)
+        prob= count/count.sum()
+        return 1 - np.sum(prob ** 2)
+    def _split(self,x, y,feat,sol):
+        indexSin= x[:,feat]<= sol
+        indexDex= x[:,feat]> sol
+        return (x[indexSin],y[indexSin],x[indexDex],y[indexDex])
+    
